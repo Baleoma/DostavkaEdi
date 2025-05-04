d9bb21a4de53ff403e434814bbf6fb0f3553c715
Baleoma <quantumqueen123@gmail.com>
2025-05-05 02:14:27 +0700

    Statuses CRUD (not tested)

---
+++ app/Http/Controllers/Api/StatusController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StatusRequest;
+use App\Http\Resources\StatusResource;
+use App\Models\Status;
+use Illuminate\Http\Request;
+
+class StatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Status::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StatusRequest $request)
+    {
+        $created_status = Status::create($request->validated());
+
+        return new StatusResource($created_status);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return new StatusResource(Status::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StatusRequest $request, Status $status)
+    {
+        $status->update($request->validated());
+
+        return new StatusResource($status);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Status $status)
+    {
+        $status->delete();
+        return response(null, 204);
+    }
+}
