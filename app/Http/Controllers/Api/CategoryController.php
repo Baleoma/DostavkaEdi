f27363535864749ce8732f373f5f28ab76459661
Baleoma <quantumqueen123@gmail.com>
2025-05-04 23:08:38 +0700

    Auth + Categories CRUD

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CategoryRequest $request)
+    {
+        $created_category = Category::create($request->validated());
+
+        return new CategoryResource($created_category);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return new CategoryResource(Category::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CategoryRequest $request, Category $category)
+    {
+        $category->update($request->validated());
+
+        return new CategoryResource($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response(null, 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Публичные маршруты (без авторизации)
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+
+// Защищенные маршруты (с авторизацией)
+Route::middleware('auth:sanctum')->group(function () {
+    // Информация о текущем пользователе
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+// Ресурсы API
+    Route::apiResources([
+        'categories' => CategoryController::class,
+    ]);
+
+    // Выход из системы
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
