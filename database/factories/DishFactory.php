d5f730f7051a796133943792bac5208f1ec776d5
Baleoma <quantumqueen123@gmail.com>
2025-05-04 14:26:56 +0700

    Seeders

--- database/factories/DishFactory.php
+++ database/factories/DishFactory.php
@@ -17,7 +17,12 @@ class DishFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->word(),
+            'description' => $this->faker->paragraph(3),
+            'price' => $this->faker->randomDigit(),
+            'image' => $this->faker->imageUrl($width = 200, $height = 200),
+            'restaurant_id' => $this->faker->numberBetween(1, 5),
+            'category_id' => $this->faker->numberBetween(1, 5),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Discount;
+use App\Models\Dish;
+use App\Models\Restaurant;
 use App\Models\Role;
+use App\Models\Status;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,7 +19,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         Role::factory()->createMany([
             ['name' => 'admin'],
@@ -28,5 +32,25 @@ class DatabaseSeeder extends Seeder
             'phone' => +7228133778,
             'role_id' => 1,
         ]);
+
+        Discount::factory()->create([
+            'min_order_amount' => 2000,
+            'discount_percentage' => 5,
+        ]);
+
+        Status::factory()->createMany([
+            ['name' => 'pending'],
+            ['name' => 'confirmed'],
+            ['name' => 'in_progress'],
+            ['name' => 'delivered'],
+            ['name' => 'cancelled'],
+        ]);
+
+        User::factory(10)->create();
+        Restaurant::factory(10)->create();
+        Category::factory(10)->create();
+        Dish::factory(10)->create();
+
+
     }
 }
